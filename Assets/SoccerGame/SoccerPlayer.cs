4d73009462503ebfb8a4440ae3f9577f37f3183c
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-05-15 11:57:21 +0300

    Debug Target Direction

---
+++ Assets/SoccerGame/SoccerPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerPlayer : MonoBehaviour {
+
+    public GameObject tacticTarget;
+    public GameObject tacticPoint;
+
+	// Use this for initialization
+	void Start () {
+        tacticTarget = GameObject.FindGameObjectWithTag("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(tacticTarget) Debug.DrawLine(projectPointOntoFloor(transform.position), projectPointOntoFloor(tacticTarget.transform.position), Color.yellow);
+		
+	}
+
+    Vector3 projectPointOntoFloor(Vector3 pointToProject) {
+        Vector3 projectedPoint = new Vector3(pointToProject.x, 0, pointToProject.z);
+        
+        return projectedPoint;
+    }
+}
