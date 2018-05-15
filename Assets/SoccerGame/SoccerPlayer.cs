45c96f901c3843e30ea9b754ad0d437ca8a79188
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-05-15 15:15:11 +0300

    Calculate ball touch point

--- Assets/SoccerGame/SoccerPlayer.cs
+++ Assets/SoccerGame/SoccerPlayer.cs
@@ -4,30 +4,37 @@ using UnityEngine;
 
 public class SoccerPlayer : MonoBehaviour {
 
-    public GameObject tacticTarget;
-    public GameObject tacticPoint;
+    public GameObject soccerBall;
+    public GameObject dribbleTarget;
+    public Vector3 ballKickVector;
+    public Vector3 ballTouchPoint;
+
+    public float ballTouchDistance = 0.1F;
 
 	// Use this for initialization
 	void Start () {
-        tacticTarget = GameObject.FindGameObjectWithTag("Ball");
+        soccerBall = GameObject.FindGameObjectWithTag("Ball");
         Debug.Log(new Vector3(2,2,2) - new Vector3(3,3,3));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (tacticTarget) {
-            Vector3 tacticTargetVector = projectPointOntoFloor(transform.position) - projectPointOntoFloor(tacticTarget.transform.position);
-            Debug.DrawRay(projectPointOntoFloor(tacticTarget.transform.position), tacticTargetVector, Color.green);
+        if (soccerBall) {
+            Vector3 tacticTargetVector = projectPointOntoFloor(transform.position) - projectPointOntoFloor(soccerBall.transform.position);
+            Debug.DrawRay(projectPointOntoFloor(soccerBall.transform.position), tacticTargetVector, Color.green);
 
-            if (tacticPoint){
-                Vector3 tacticPointVector = projectPointOntoFloor(tacticPoint.transform.position) - projectPointOntoFloor(tacticTarget.transform.position);
-                Debug.DrawRay(projectPointOntoFloor(tacticTarget.transform.position), tacticPointVector.normalized, Color.yellow);
+            if (dribbleTarget){
+                Vector3 tacticPointVector = projectPointOntoFloor(dribbleTarget.transform.position) - projectPointOntoFloor(soccerBall.transform.position);
+                ballKickVector = Vector3.ClampMagnitude(tacticPointVector, ballTouchDistance) * -1;
+                ballTouchPoint = soccerBall.transform.position + ballKickVector;
+                Debug.DrawRay(projectPointOntoFloor(soccerBall.transform.position), ballKickVector, Color.yellow);
             }
         }
+    }
 
-
-
-
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(ballTouchPoint, "Light Gizmo.tiff", true);
     }
 
     Vector3 projectPointOntoFloor(Vector3 pointToProject) {
