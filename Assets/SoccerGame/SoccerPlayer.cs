43ed30b460f8c01fff4582567d6e9f281a1228b9
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-05-17 19:41:39 +0300

    Implement Dribble Turn Angle and Ball Touch Distance dependency

--- Assets/SoccerGame/SoccerPlayer.cs
+++ Assets/SoccerGame/SoccerPlayer.cs
@@ -7,27 +7,36 @@ public class SoccerPlayer : MonoBehaviour {
     public GameObject soccerBall;
     public GameObject dribbleTarget;
     public GameObject moveTarget;
+    public GameObject ballProjection;
+    public GameObject tacticPoint;
     public Vector3 ballKickVector;
     public Vector3 ballTouchPoint;
 
     public float ballTouchDistance = 0.1F;
 
+    public int ballTouchDistanceCoeff = 350;
+
     public bool dribbleStraightforward = false;
 
     // Use this for initialization
     void Start () {
+        ballProjection = GameObject.FindGameObjectWithTag("BallProjection");
+        tacticPoint = GameObject.FindGameObjectWithTag("TacticPoint");
         soccerBall = GameObject.FindGameObjectWithTag("Ball");
         moveTarget = GameObject.FindGameObjectWithTag("Target");
-        Debug.Log(new Vector3(2,2,2) - new Vector3(3,3,3));
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (soccerBall) {
             Vector3 tacticTargetVector = ProjectPointOntoFloor(transform.position, 0) - ProjectPointOntoFloor(soccerBall.transform.position, 0);
+            float maneuverAngle = Vector3.SignedAngle(soccerBall.transform.position - transform.position, tacticPoint.transform.position - soccerBall.transform.position, Vector3.up);
+
             Debug.DrawRay(ProjectPointOntoFloor(soccerBall.transform.position, 0), tacticTargetVector, Color.green);
 
-            if (dribbleTarget){
+            ballTouchDistance = Mathf.Abs(maneuverAngle) / ballTouchDistanceCoeff;
+
+            if (dribbleTarget) {
                 Vector3 tacticPointVector = ProjectPointOntoFloor(dribbleTarget.transform.position, 0) - ProjectPointOntoFloor(soccerBall.transform.position, 0);
                 ballKickVector = Vector3.ClampMagnitude(tacticPointVector, ballTouchDistance) * -1;
                 ballTouchPoint = soccerBall.transform.position + ballKickVector;
@@ -36,6 +45,17 @@ public class SoccerPlayer : MonoBehaviour {
                 else moveTarget.transform.position = ProjectPointOntoFloor(soccerBall.transform.position, 0.01F);
 
                 Debug.DrawRay(ProjectPointOntoFloor(soccerBall.transform.position, 0), ballKickVector, Color.yellow);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("Dribble Straight Forward");
+                dribbleStraightforward = true;
+            }
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)))
+            {
+                Debug.Log("Dribble Turns");
+                dribbleStraightforward = false;
             }
         }
 
