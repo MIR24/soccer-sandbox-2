1170b2f87740bfd2f2785e0830ae3ad953761459
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-05-20 12:27:44 +0300

    Control ball velocity

--- Assets/BallPhysics.cs
+++ Assets/BallPhysics.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BallPhysics : MonoBehaviour {
     public GameObject tacticPoint;
     public float forceClamp = 1.2F;
+    public float maxSpeed = 1f;
+
+    private Rigidbody myRigidBody;
 
     // Use this for initialization
     void Start () {
         tacticPoint = GameObject.FindGameObjectWithTag("TacticPoint");
+        myRigidBody = gameObject.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
@@ -19,9 +23,15 @@ public class BallPhysics : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.transform.root.gameObject.tag);
-        if(collision.gameObject.transform.root.gameObject.tag == "Player")
-            gameObject.GetComponent<Rigidbody>().AddForce((tacticPoint.transform.position-transform.position).normalized/forceClamp, ForceMode.Acceleration);
+        if (collision.gameObject.transform.root.gameObject.tag == "Player")
+            gameObject.GetComponent<Rigidbody>().AddForce((tacticPoint.transform.position - transform.position).normalized / forceClamp, ForceMode.Acceleration);
     }
 
-
+    private void FixedUpdate()
+    {
+        if (myRigidBody.velocity.magnitude > maxSpeed)
+        {
+            myRigidBody.velocity = Vector3.ClampMagnitude(myRigidBody.velocity, maxSpeed);
+        }
+    }
 }
