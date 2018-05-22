d5a8641d35e0dfc653b6cc5a6d97247fd0f4a34e
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-05-23 00:39:31 +0300

    Remove comments

--- Assets/SoccerGame/Scripts/BallPhysics.cs
+++ Assets/SoccerGame/Scripts/BallPhysics.cs
@@ -9,20 +9,16 @@ public class BallPhysics : MonoBehaviour {
     public float forceClamp = 1.2F;
     public float maxSpeed = 1f;
     public float goalKickMaxSpeed = 50f;
-    //public string performingGoalKick;
     public float goalKickPower=2F;
 
     private Rigidbody myRigidBody;
 
-    // Use this for initialization
     void Start () {
         tacticPoint = GameObject.FindGameObjectWithTag("TacticPoint");
         goalKickPoint = GameObject.Find("GoalKickDirection");
         myRigidBody = gameObject.GetComponent<Rigidbody>();
-        //MessageDispatcher.AddListener("GOAL_KICK", OnGoalKick, true);
     }
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -36,31 +32,8 @@ public class BallPhysics : MonoBehaviour {
         if (collision.gameObject.transform.root.gameObject.tag == "Player")
         {
             MessageDispatcher.SendMessage(this, "BALL_HIT", collision.gameObject.transform.root.gameObject.name, 0);
-
-            //if (collision.gameObject.transform.root.gameObject.name == performingGoalKick)
-            //{
-            //    Vector3 goalKickDirection = goalKickPoint.transform.position - transform.position;
-            //    Debug.DrawRay(transform.position, goalKickDirection, Color.yellow, 3F);
-            //    myRigidBody.AddForce(goalKickDirection.normalized * goalKickPower, ForceMode.Impulse);
-            //    maxSpeed = goalKickMaxSpeed;
-            //    Debug.Log("Applied force for goal kick");
-            //}
-            //else
-            //{
-            //    Vector3 goalKickDirection = tacticPoint.transform.position - transform.position;
-            //    myRigidBody.AddForce(goalKickDirection.normalized / forceClamp, ForceMode.Acceleration);
-            //}
         }
     }
-
-    //void OnGoalKick(IMessage rMessage)
-    //{
-    //    Debug.Log((string)rMessage.Data + " performing goal kick");
-    //    if (performingGoalKick != null)
-    //    {
-    //        performingGoalKick = (string)rMessage.Data;
-    //    }
-    //}
 
     private void FixedUpdate()
     {
